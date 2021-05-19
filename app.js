6cb34180589b5743ced245d7bd13b9826688c607
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-05-19 16:48:16 -0400

    Add eraser state toggle button

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const canvasGrid = document.querySelector('.canvas__grid');
 //Control Varriables
 const gridSizeSlider = document.querySelector('.input__slider');
 const eraseCanvasButton = document.querySelector('.btn__clear');
+const eraseCellButton = document.querySelector('.btn_eraser');
+let eraseActive;
 
 
 function createCanvasCells (gridSize) {
@@ -39,13 +41,28 @@ function eraseCanvas() {
 
 //Event listeners
 window.onload = createCanvasCells; 
-gridSizeSlider.addEventListener('mouseup',createCanvasCells);
+gridSizeSlider.addEventListener('mouseup', createCanvasCells);
 eraseCanvasButton.addEventListener('click', eraseCanvas);
+
 
 //Target grid children with event delegation
 canvasGrid.addEventListener('mouseover', function (e) {
-    // Add paint class to apply background via CSS
-    if (e.target.matches('.canvas__cell')) {
-      e.target.classList.add('paint');
+    // Add paint class to apply background via CSS and 'paint cell'
+    if (e.target.matches('.canvas__cell') && eraseActive){
+      e.target.classList.remove('paint');
+    } else {
+        e.target.classList.add('paint');
     }
   });
+
+//Target button child with event delegation to toggle active class
+eraseCellButton.addEventListener('click', function (e) {
+  if (e.target.matches('.active')) {
+       e.target.classList.remove('active');
+       eraseActive = false;
+    } else {
+        e.target.classList.add('active');
+        eraseActive = true;
+    }
+});
+  
