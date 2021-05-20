d5b996fdd76c0e348d04272f67649c1cc6be6423
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-05-20 17:12:49 -0400

    Refractor

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const eraseCellButton = document.querySelector('.btn__eraser');
 const canvasColorPicker = document.querySelector('#canvas-color');
 const brushColorPicker = document.querySelector('#brush-color');
 const rainbowButton = document.querySelector('.btn__rainbow');
+const shaderButton = document.querySelector('.btn_shader');
 let eraseActive;
 let rainbowActive;
 
@@ -16,6 +17,9 @@ function createCanvasCells (gridSize) {
     gridSize = gridSizeSlider.value    
 //Clear cells on resize
     clearCanvas();
+//Toggle rainbow or eraser button off on resize 
+    setEraserBtnOff();
+    setRainbowBtnOff();
     for (let i = 0; i < (gridSize * gridSize); i++) {
         let canvasCell = document.createElement('div');
         canvasCell.classList = 'canvas__cell';
@@ -31,6 +35,25 @@ function clearCanvas() {
             currentCell.remove();
         });
     }
+};
+
+function paintCells (e) {
+// Remove paint class to erase "painted cells'
+    if (e.target.matches('.canvas__cell') && eraseActive){
+        e.target.classList.remove('paint');
+        e.target.style.background = null;
+    // Add paint class to apply background via CSS and 'paint cell'
+    } else {
+        e.target.classList.add('paint');
+        if(rainbowActive) {
+            let r = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            let g = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            let b = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+        } else {
+            e.target.style.background = brushColorPicker.value;
+        };
+    };
 };
 
 function eraseCanvas() {
@@ -56,45 +79,23 @@ function setEraserBtnOff() {
     eraseActive = false;
 };
 
+function toggleEraseCell(e) {
+    if (e.target.matches('.active')) {
+        setEraserBtnOff();
+     } else {
+         e.target.classList.add('active');
+         eraseActive = true;
+         //Toogle rainbow button off when eraser is on
+         setRainbowBtnOff();
+     };
+};
+
 function setRainbowBtnOff() {
     rainbowButton.classList.remove('active');
     rainbowActive = false;
 };
 
-//Event listeners
-window.onload = createCanvasCells; 
-gridSizeSlider.addEventListener('mouseup', createCanvasCells);
-eraseCanvasButton.addEventListener('click', eraseCanvas);
-canvasColorPicker.addEventListener('change', selectCanvasColor);
-canvasColorPicker.addEventListener('input', selectCanvasColor);
-
-
-//Target grid children with event delegation
-canvasGrid.addEventListener('mouseover', function (e) {
-    // Remove paint class to erase "painted cells'
-    if (e.target.matches('.canvas__cell') && eraseActive){
-      e.target.classList.remove('paint');
-      e.target.style.background = null;
-    // Add paint class to apply background via CSS and 'paint cell'
-    } else {
-        e.target.classList.add('paint');
-        e.target.style.background = brushColorPicker.value;
-    };
-  });
-
-//Event delgegation to toggle active class
-eraseCellButton.addEventListener('click', function (e) {
-  if (e.target.matches('.active')) {
-       setEraserBtnOff();
-    } else {
-        e.target.classList.add('active');
-        eraseActive = true;
-        //Toogle rainbow button off when eraser is on
-        setRainbowBtnOff();
-    };
-});
-  
-rainbowButton.addEventListener('click', function (e) {
+function toggleRainbow(e) {
     if (e.target.matches('.active')) {
         setRainbowBtnOff();
       } else {
@@ -103,4 +104,15 @@ rainbowButton.addEventListener('click', function (e) {
           //Toogle eraser button off when rainbow is on
          setEraserBtnOff();
       };
-  });
+}
+
+
+//Event listeners
+window.onload = createCanvasCells; 
+gridSizeSlider.addEventListener('mouseup', createCanvasCells);
+eraseCanvasButton.addEventListener('click', eraseCanvas);
+canvasColorPicker.addEventListener('change', selectCanvasColor);
+canvasColorPicker.addEventListener('input', selectCanvasColor);
+canvasGrid.addEventListener('mouseover', paintCells);
+eraseCellButton.addEventListener('click', toggleEraseCell);
+rainbowButton.addEventListener('click', toggleRainbow);
